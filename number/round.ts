328a2fd5ee0ae707d987f8b44a010d9d98ef4dcc
Iurii Raboshuk <iurii.raboshuk@gmail.com>
2020-04-15 18:15:59 +0300

    Fix round() function issue with floating point value precision

--- number/round.ts
+++ number/round.ts
@@ -1,4 +1,6 @@
 export function round(n: number, digits: number = 0) {
     const multiplier = Math.pow(10, digits);
-    return Math.round(n * multiplier) / multiplier;
+    // Fix issues with floating point operations when e.g. 2.5 could be stored as 2.4999999993
+    // Which would cause it to round to 2 instead of 3
+    return Math.round(Math.round(n * multiplier * 10) / 10) / multiplier;
 }
